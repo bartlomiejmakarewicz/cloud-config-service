7c22a7bb5c715ea8c5284fd5f6f4fff0a052fc1f
Bartlomiej Makarewicz <bartlomiej.makarewicz@gmail.com>
2019-07-16 20:57:17 +0200

    Create cloud config app

---
+++ src/main/kotlin/com/example/cloudconfigservice/CloudConfigServiceApplication.kt
@@ -0,0 +1,15 @@
+package com.example.cloudconfigservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+class CloudConfigServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CloudConfigServiceApplication>(*args)
+}
